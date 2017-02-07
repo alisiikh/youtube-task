cc434affa606b85e2110b96e55f12c667ca51773
Aleksey Lisikh <lial@ciklum.com>
2017-02-08 00:13:18 +0200

    Added method for getting channel's information

--- src/main/java/com/alisiikh/service/YouTubeService.java
+++ src/main/java/com/alisiikh/service/YouTubeService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,24 +27,70 @@ public class YouTubeService implements IYouTubeService {
 	private static final Logger LOG = LoggerFactory.getLogger(YouTubeService.class);
 
 	private static final Pattern DURATION_PATTERN = Pattern.compile("PT(\\d+)M(\\d+)S");
+
 	@Override
 	public YouTubeVideoInfo getVideoInfo(String videoId) {
-		Validate.notBlank(videoId);
+		Validate.notBlank(videoId, "Video id is required!");
 
 		try {
-			Document doc = Jsoup.connect("https://www.youtube.com/watch?v=" + videoId).get();
+			Document doc = fetchDocument("https://www.youtube.com/watch?v=" + videoId);
 
 			return gatherVideoInfo(doc);
 		} catch (IOException e) {
-			LOG.debug("Failed to find video on youtube website");
+			LOG.debug("Exception occurred during fetching video info");
 			return null;
 		}
 	}
 
 	@Override
 	public YouTubeChannelInfo getChannelInfo(String channelId) {
-		// TODO: Implement channel info fetching
-		return new YouTubeChannelInfo();
+		Validate.notBlank(channelId, "Channel id is required!");
+
+		try {
+			Document doc = fetchDocument("https://www.youtube.com/channel/" + channelId + "/about");
+
+			return gatherChannelInfo(doc);
+		} catch (IOException e) {
+			LOG.debug("Exception occurred during fetching channel info");
+			return null;
+		}
+	}
+
+	private YouTubeChannelInfo gatherChannelInfo(Document doc) {
+		YouTubeChannelInfo channelInfo = new YouTubeChannelInfo();
+		String channelId = doc.select("meta[itemprop='channelId']").attr("content");
+		String url = doc.select("link[itemprop='url']").attr("href");
+
+		Elements channelAttrs = doc.select("#browse-items-primary .about-metadata-stats .about-stats");
+		String subscribers = channelAttrs.select(".about-stat:nth-of-type(1) b").text()
+				.replaceAll(",", "")
+				.trim();
+		String totalViews = channelAttrs.select(".about-stat:nth-of-type(2) b").text()
+				.replaceAll(",", "")
+				.trim();
+		String registrationDate = channelAttrs.select(".about-stat:nth-of-type(3)").text()
+				.replaceFirst("Joined", "")
+				.trim();
+
+		channelInfo.setId(channelId);
+		channelInfo.setUrl(url);
+		channelInfo.setSubscribers(Integer.valueOf(subscribers));
+		channelInfo.setViews(Integer.valueOf(totalViews));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy", Locale.US);
+		try {
+			channelInfo.setRegistrationDate(sdf.parse(registrationDate).getTime());
+		} catch (ParseException e) {
+			// nothing
+		}
+
+		return channelInfo;
+	}
+
+	private Document fetchDocument(String url) throws IOException {
+		return Jsoup.connect(url)
+				.header("Accept-Language", "en-US")
+				.get();
 	}
 
 	private YouTubeVideoInfo gatherVideoInfo(Document doc) {
