0d1c1e7d44ddd4542bf4d906bc4618fada5a48b8
Aleksey Lisiikh <lial@ciklum.com>
2017-02-09 12:33:00 +0200

    Fixed issue with channel videos fetching, refactored some exceptions handling

--- src/main/java/com/alisiikh/service/YouTubeService.java
+++ src/main/java/com/alisiikh/service/YouTubeService.java
@@ -23,8 +23,10 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.Matcher;
@@ -32,6 +34,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static com.alisiikh.util.UrlUtils.encode;
+
+
 /**
  * @author lial
  */
@@ -42,6 +46,9 @@ public class YouTubeService implements IYouTubeService {
 
 	private static final Pattern DURATION_PATTERN = Pattern.compile("PT(\\d+)M(\\d+)S");
 	private static final String YOUTUBE_WEBSITE_ADDRESS = "https://www.youtube.com";
+	private static final DateTimeFormatter PUBLISHED_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.US);
+	private static final DateTimeFormatter REGISTERED_DATE_FORMAT = DateTimeFormatter.ofPattern("MMM dd, yyyy",
+			Locale.US);
 
 	private RestTemplate restTemplate;
 	private ThreadPoolTaskExecutor taskExecutor;
@@ -66,35 +73,43 @@ public class YouTubeService implements IYouTubeService {
 			return findVideoInfo(doc);
 		} catch (IOException e) {
 			LOG.warn("Exception occurred during fetching video info", e);
-			return null;
+			throw new YouTubeDataFetchException();
 		}
 	}
 
-	private <T> Optional<T> readDataFromYoutube(String url) throws IOException {
+	private <T> Optional<T> readDataFromYoutube(String relativeUrl) throws IOException {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Accept-Language", "en-US");
 
 		HttpEntity<String> httpEntity = new HttpEntity<>("parameters", headers);
+		ResponseEntity<String> jsonData;
+
+		String requestUrl = YOUTUBE_WEBSITE_ADDRESS + relativeUrl;
 		try {
-			ResponseEntity<String> jsonData = restTemplate.exchange(YOUTUBE_WEBSITE_ADDRESS + url, HttpMethod.GET, httpEntity, String.class);
+			jsonData = restTemplate.exchange(requestUrl, HttpMethod.GET, httpEntity, String.class);
 
-			JSONParser jsonParser = new JSONParser();
-
-			try {
-				@SuppressWarnings("unchecked")
-				T json = (T) jsonParser.parse(jsonData.getBody());
-				return Optional.of(json);
-			} catch (org.json.simple.parser.ParseException e) {
-				LOG.warn("Failed to parse JSON data passed by YouTube.");
-				return Optional.empty();
-			}
 		} catch (HttpClientErrorException e) {
 			if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+				LOG.debug("Requested entity couldn't be found");
 				throw new YouTubeEntityNotFoundException("Failed to find anything against the following url: "
-						+ YOUTUBE_WEBSITE_ADDRESS + url);
+						+ requestUrl);
 			} else {
-				throw new YouTubeDataFetchException();
+				LOG.warn("Unknown error occurred during communicating with YouTube", e);
+				throw new YouTubeDataFetchException(e);
 			}
+		}
+
+		JSONParser jsonParser = new JSONParser();
+
+		try {
+			@SuppressWarnings("unchecked")
+			T json = (T) jsonParser.parse(jsonData.getBody());
+
+			return Optional.of(json);
+		} catch (org.json.simple.parser.ParseException e) {
+			LOG.warn("Failed to parse JSON data passed by YouTube.", e);
+
+			return Optional.empty();
 		}
 	}
 
@@ -140,7 +155,7 @@ public class YouTubeService implements IYouTubeService {
 			taskExecutor.submit(() -> {
 				try {
 					Optional<JSONObject> jsonObject = readDataFromYoutube("/channel/" + channelId
-							+ "/videos?sort=da&flow=grid&view=0&spf=navigate");
+							+ "/videos?sort=p&flow=grid&view=0&spf=navigate");
 					if (!jsonObject.isPresent()) {
 						throw new YouTubeDataFetchException("Failed to read data from YouTube");
 					}
@@ -160,6 +175,7 @@ public class YouTubeService implements IYouTubeService {
 			try {
 				latch.await(20, TimeUnit.SECONDS);
 			} catch (InterruptedException e) {
+				LOG.debug("Failed to finish channel's most popular videos search");
 				throw new YouTubeDataFetchException(e);
 			}
 
@@ -172,12 +188,12 @@ public class YouTubeService implements IYouTubeService {
 		int currentSize = videoBlocks.size();
 
 		if (currentSize < size) {
-			String moreVideosHref = doc.select("#browse-items-primary .load-more-button")
+			String loadMoreVideosHref = doc.select("#browse-items-primary .load-more-button")
 					.attr("data-uix-load-more-href");
 
 			while (currentSize < size) {
 				try {
-					Optional<JSONObject> jsonObj = readDataFromYoutube(moreVideosHref);
+					Optional<JSONObject> jsonObj = readDataFromYoutube(loadMoreVideosHref);
 					if (!jsonObj.isPresent()) {
 						throw new YouTubeDataFetchException();
 					}
@@ -185,7 +201,7 @@ public class YouTubeService implements IYouTubeService {
 					String contentHtml = (String) jsonObj.get().get("content_html");
 					String moreVideosButtonHtml = (String) jsonObj.get().get("load_more_widget_html");
 
-					moreVideosHref = Jsoup.parseBodyFragment(moreVideosButtonHtml).body().select(".load-more-button")
+					loadMoreVideosHref = Jsoup.parseBodyFragment(moreVideosButtonHtml).body().select(".load-more-button")
 							.attr("data-uix-load-more-href");
 
 					Element extraVideosBody = Jsoup.parseBodyFragment(contentHtml).body();
@@ -199,15 +215,12 @@ public class YouTubeService implements IYouTubeService {
 			}
 		}
 
-		return fetchVideosInfo(videoBlocks.subList(0, size));
-	}
-
-	private List<YouTubeVideoInfo> fetchVideosInfo(List<Element> videoBlocks) {
-		return videoBlocks.parallelStream().map((block) -> {
-			String videoId = block.select("> .yt-lockup-video").attr("data-context-item-id");
+		return videoBlocks.subList(0, size).parallelStream().map((block) -> {
+			String videoId = block.select("> .yt-lockup-video")
+					.attr("data-context-item-id");
 
 			return getVideoInfo(videoId);
-		}).filter(Objects::nonNull).collect(Collectors.toList());
+		}).collect(Collectors.toList());
 	}
 
 	private YouTubeChannelInfo findChannelInfo(Document doc) {
@@ -220,18 +233,20 @@ public class YouTubeService implements IYouTubeService {
 		String totalViews = channelAttrs.select(".about-stat:nth-of-type(2) b").text()
 				.replaceAll(",", "")
 				.trim();
-		String registrationDate = channelAttrs.select(".about-stat:nth-of-type(3)").text()
+		String dateRegistered = channelAttrs.select(".about-stat:nth-of-type(3)").text()
 				.replaceFirst("Joined", "")
 				.trim();
 
 		channelInfo.setSubscribers(Integer.valueOf(subscribers));
 		channelInfo.setViews(Integer.valueOf(totalViews));
 
-		SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy", Locale.US);
 		try {
-			channelInfo.setRegistrationDate(sdf.parse(registrationDate).getTime());
-		} catch (ParseException e) {
-			LOG.debug("Failed to parse registration date string: " + registrationDate);
+			LocalDate registeredDate = LocalDate.parse(dateRegistered, REGISTERED_DATE_FORMAT);
+			long registeredDateMillis = registeredDate.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000;
+
+			channelInfo.setRegistrationDate(registeredDateMillis);
+		} catch (DateTimeParseException e) {
+			LOG.debug("Failed to parse registration date string: " + dateRegistered);
 		}
 
 		return channelInfo;
@@ -260,11 +275,13 @@ public class YouTubeService implements IYouTubeService {
 		videoInfo.setViews(Integer.valueOf(views));
 		videoInfo.setUrl(url);
 
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		try {
-			videoInfo.setPublishedDate(sdf.parse(datePublished).getTime());
-		} catch (ParseException e) {
-			// nothing
+			LocalDate publishedDate = LocalDate.parse(datePublished, PUBLISHED_DATE_FORMAT);
+			long publishedDateMillis = publishedDate.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000;
+
+			videoInfo.setPublishedDate(publishedDateMillis);
+		} catch (DateTimeParseException e) {
+			LOG.debug("Failed to parse published date of the video");
 		}
 
 		return videoInfo;
